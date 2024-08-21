96c707841060a318a199cc883bad4d345985d571
Alumno <Alumno@LAB1507-06>
2024-08-20 19:51:23 -0500

    Agregar archivos de proyecto.

---
+++ Demo01B/Persona.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01B
+{
+    public class Persona
+    {
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public int Edad { get; set; }
+
+        public string NombresCompletos { get; set; }
+
+        public string EsMayorEdad()
+        {
+            string mensaje = "Es menor de edad";
+            if (Edad > 18)
+
+                mensaje = "Es mayor de edad";
+
+            return mensaje;
+
+        public void AsignarNombresCompletos()
+        {
+                NombresCompletos = Nombres + " " + Apellidos;
+
+        }
+
+        }
+
+    }
+}
---
+++ Demo01B/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Persona persona = new Persona();
+            persona.Nombres = "Jeremy Emanuel";
+            persona.Apellidos = "Bellido Ñañez";
+
+            Persona persona1 = new Persona
+            {
+                Nombres = "David",
+                Apellidos = "Rey",
+                Edad = 16
+        };
+
+            Triangulo triangulo = new Triangulo();
+            triangulo.Base = 5;
+            triangulo.Altura = 8;
+
+            triangulo.CalcularArea();
+
+            Console.WriteLine(triangulo.CalcularArea());
+
+            Console.WriteLine("Hola Mundo");
+            Console.Read();
+        }
+    }
+}
